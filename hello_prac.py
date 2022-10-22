679d2783f7586bd8a2225f2ff95f52fb6703da4c
ysj <suk921@gmail.com>
2022-10-22 20:11:45 +0900

    for loop grammar

--- hello_prac.py
+++ hello_prac.py
@@ -1,9 +1,12 @@
-def oddeven(num):
-    if num % 2 == 0:
-        return True
-    else:
-        return False
+people = [{'name': 'bob', 'age': 27},
+          {'name': 'carry', 'age': 30}]
 
 
-result = oddeven(20)
-print(result)
+def get_age(myname):
+    for person in people:
+        if person['name'] == myname:
+            return person['age']
+        return '해당하는 이름이 없습니다'
+
+
+print(get_age('bob'))
